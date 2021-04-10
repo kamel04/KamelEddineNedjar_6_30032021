9047284628a877bc635c689762e8337c855ceb94
kamel04 <nedjar.k@gmail.com>
2021-04-10 16:26:00 +0200

    JS page home

---
+++ public/script/mainContent.js
@@ -0,0 +1,4 @@
+const skipeToContent = document.querySelector(".skipToContent");
+const photographerGallery = document.getElementById("photographer-gallery");
+const tagSelectList = document.getElementById("nav-taglist");
+const tagSelectListChild = tagSelectList.children;
